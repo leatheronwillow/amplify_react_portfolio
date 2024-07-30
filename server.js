0c7562e6b731add070c1d03707df472b6ae8a4bc
Mohammad Hassan Azad <m.h.azad-14@outlook.com>
2024-07-30 16:31:36 +0100

    deploying to render

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const cors = require("cors");
 const nodemailer = require("nodemailer");
+const PORT = process.env.PORT || 5000;
 
 // server used to send send emails
 const app = express();
